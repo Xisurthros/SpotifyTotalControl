befd0eea02a180b3308e15ba8b5dcbb0e7edfd26
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-08-08 11:51:18 -0500

    Added get-current-track to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests, time
+from pprint import pprint
 from device import Device
 from refresh import Refresh
 
@@ -13,6 +14,43 @@ VOLUME = 'https://api.spotify.com/v1/me/player/volume'
 TOP = '	https://api.spotify.com/v1/me/top/'
 
 DEVICE_ID = Device().device()
+
+def get_current_track():
+	print('Ctr-C to get out of currently-playing\n')
+	current_track_id = None
+	while True:
+		try:
+			response = requests.get(
+				SPOTIFY_GET_CURRENT_TRACK_URL,
+				headers={
+				    "Authorization": f"Bearer {ACCESS_TOKEN}"
+				}
+			)
+			json_resp = response.json()
+		
+			track_id = json_resp['item']['id']
+			track_name = json_resp['item']['name']
+			artists = [artist for artist in json_resp['item']['artists']]
+		
+			link = json_resp['item']['external_urls']['spotify']
+		
+			artist_names = ', '.join([artist['name'] for artist in artists])
+		
+			current_track_info = {
+				"id": track_id,
+				"track_name": track_name,
+				"artists": artist_names,
+				"link": link
+			}
+	
+			if current_track_info['id'] != current_track_id:
+				pprint(
+					current_track_info,
+					indent=4,
+				)
+				current_track_id = current_track_info['id']
+		except KeyboardInterrupt:
+			break
 
 def next():
 	requests.post(
@@ -71,6 +109,8 @@ def main():
 			play()
 		elif user_input == 'volume':
 			volume()
+		elif user_input == 'current':
+			get_current_track()
 
 if __name__ == '__main__':
 	ACCESS_TOKEN = str(Refresh().refresh())
